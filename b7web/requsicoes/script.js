a9f0cb8a4aa1a2abf611f4b82d80ed075507d51a
Ana Beatriz Silva Araújo <135762449+anaaraujo27@users.noreply.github.com>
2024-07-19 12:59:26 -0300

    novo

--- b7web/requsicoes/script.js
+++ b7web/requsicoes/script.js
@@ -1,3 +1,37 @@
 /* 
-O que é Sincrono e Assincrono
+O que e Sincrono e Assincrono?
+
+   Sincrono - execucao por ordem de acontecimentos
+
+   Assincrona - execucoes ao mesmo tempo, sem oredem 
+*/
+
+let nome = 'Rita'
+let sobrenome = 'Oliveira'
+
+let nomeCompleto = `${nome} ${sobrenome}`
+
+/* 
+
+Como a web funciona (Requisicao e Resposta)?
+
+Request = Requisicao
+-> Cabecalhos / Headers
+-> Corpo / Body
+
+Response = Resposta
+-> Cabecalhos / Headers
+-> Corpo / Body
+
+*/
+
+/* 
+ Como funciona uma API?
+ API = Application Programming Interface 
+ -> Comunicacao entre 2 softwers
+
+ JSON = Javascript Object Notation, padrao atual de linguagem de comunicacao entre as 
+ maquinas - https://jsonplacehlder.typicode.com/posts
+ XML = antigo padrao de linguagem de comunicacao entre as maquinas
+
 */
